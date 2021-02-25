d8e0c3a982960849e3a8c4fdac7ed2a2a057aa36
Timothy <timauthx@gmail.com>
2021-02-25 02:19:04 +0200

    refactor (sensors) CSS811 logic reformat

--- readings/airQuality.go
+++ readings/airQuality.go
@@ -11,47 +11,47 @@ import (
 )
 
 const(
-	//// Registers
-	CCS811_STATUS = 0x00
-	CCS811_MEAS_MODE = 0x01
+	// // Registers
+	CCS811_STATUS          = 0x00
+	CCS811_MEAS_MODE       = 0x01
 	CCS811_ALG_RESULT_DATA = 0x02
-	CCS811_RAW_DATA = 0x03
-	CCS811_ENV_DATA = 0x05
-	CCS811_NTC = 0x06
-	CCS811_THRESHOLDS = 0x10
-	CCS811_BASELINE = 0x11
-	CCS811_HW_ID = 0x20
-	CCS811_HW_VERSION = 0x21
+	CCS811_RAW_DATA        = 0x03
+	CCS811_ENV_DATA        = 0x05
+	CCS811_NTC             = 0x06
+	CCS811_THRESHOLDS      = 0x10
+	CCS811_BASELINE        = 0x11
+	CCS811_HW_ID           = 0x20
+	CCS811_HW_VERSION      = 0x21
 	CCS811_FW_BOOT_VERSION = 0x23
-	CCS811_FW_APP_VERSION = 0x24
-	CCS811_ERROR_ID = 0xE0
-	CCS811_SW_RESET = 0xFF
-	//// Bootloader Registers
-	CCS811_BOOTLOADER_APP_ERASE = 0xF1
-	CCS811_BOOTLOADER_APP_DATA = 0xF2
+	CCS811_FW_APP_VERSION  = 0x24
+	CCS811_ERROR_ID        = 0xE0
+	CCS811_SW_RESET        = 0xFF
+	// // Bootloader Registers
+	CCS811_BOOTLOADER_APP_ERASE  = 0xF1
+	CCS811_BOOTLOADER_APP_DATA   = 0xF2
 	CCS811_BOOTLOADER_APP_VERIFY = 0xF3
-	CCS811_BOOTLOADER_APP_START = 0xF4
-	//// Drive mode
-	CCS811_DRIVE_MODE_IDLE = 0x00
-	CCS811_DRIVE_MODE_1SEC = 0x01
+	CCS811_BOOTLOADER_APP_START  = 0xF4
+	// // Drive mode
+	CCS811_DRIVE_MODE_IDLE  = 0x00
+	CCS811_DRIVE_MODE_1SEC  = 0x01
 	CCS811_DRIVE_MODE_10SEC = 0x02
 	CCS811_DRIVE_MODE_60SEC = 0x03
 	CCS811_DRIVE_MODE_250MS = 0x04
-	//// CONSTANTs
-	CCS811_HW_ID_CODE	=	0x81
-	CCS811_REF_RESISTOR	=	100000
-	//// STATUS - Bitwise
+	// // CONSTANTs
+	CCS811_HW_ID_CODE   = 0x81
+	CCS811_REF_RESISTOR = 100000
+	// // STATUS - Bitwise
 	CCS811_ERROR_BIT      = 0x01
 	CCS811_DATA_READY_BIT = 0x08
-	CCS811_APP_VALID_BIT         = 0x10
-	CCS811_FW_MODE_BIT           = 0x80
-	//// ERROR - Bitwise
+	CCS811_APP_VALID_BIT  = 0x10
+	CCS811_FW_MODE_BIT    = 0x80
+	// // ERROR - Bitwise
 	CCS811_WRITE_REG_INVALID = 0x01
-	CCS811_READ_REG_INVALID = 0x02
-	CCS811_MEASMODE_INVALID = 0x04
-	CCS811_MAX_RESISTANCE = 0x08
-	CCS811_HEATER_FAULT = 0x10
-	CCS811_HEATER_SUPPLY = 0x20
+	CCS811_READ_REG_INVALID  = 0x02
+	CCS811_MEASMODE_INVALID  = 0x04
+	CCS811_MAX_RESISTANCE    = 0x08
+	CCS811_HEATER_FAULT      = 0x10
+	CCS811_HEATER_SUPPLY     = 0x20
 )
 
 var (
@@ -85,7 +85,7 @@ func (s *SensorsReader) SubscribeToAirQualityReadings(addr uint8, bus int) error
 }
 
 func initCCS811(i2c *i2c.I2C) error {
-	if !verifyId(i2c) {
+	if !verifyCCS811(i2c) {
 		return fmt.Errorf("not that sensor")
 	}
 
@@ -154,10 +154,12 @@ func getStatus(d *i2c.I2C) (byte, error) {
 		return 0, err
 	}
 
+	// fmt.Printf("status: %#x\n", data[0])
+
 	return data[0], nil
 }
 
-func verifyId(d *i2c.I2C) bool{
+func verifyCCS811(d *i2c.I2C) bool{
 	data, _, err := d.ReadRegBytes(CCS811_HW_ID, 1)
 	if err == nil && data[0] == CCS811_HW_ID_CODE {
 		return true
