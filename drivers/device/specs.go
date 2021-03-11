e5ca8eac48cfa021da3012796f99973618dcc32a
Timothy <timauthx@gmail.com>
2021-03-11 02:43:53 +0200

    feat (device): implement specs generation

---
+++ drivers/device/specs.go
@@ -0,0 +1,44 @@
+package device
+
+import (
+	"github.com/timoth-y/iot-blockchain-contracts/models"
+
+	"github.com/timoth-y/iot-blockchain-sensorsys/drivers/sensors"
+	"github.com/timoth-y/iot-blockchain-sensorsys/model"
+	"github.com/timoth-y/iot-blockchain-sensorsys/shared"
+)
+
+func DiscoverSpecs() (*model.DeviceSpecs, error) {
+	var (
+		availableMetrics = make(map[models.Metric]bool)
+	)
+
+	network, err := shared.GetNetworkEnvironmentInfo(); if err != nil {
+		return nil, err
+	}
+
+	for bus, addrs := range shared.ScanI2CAddrs(0x40, 0x76) { // TODO: smart min & max addresses definition
+		for _, addr := range addrs {
+			if sf, ok := sensors.I2CSensorsMap[addr]; ok {
+				for _, metric := range sf(bus).Metrics() {
+					availableMetrics[metric] = true
+				}
+			}
+		}
+	}
+
+	var (
+		supports = make([]string, len(availableMetrics))
+		i = 0
+	)
+
+	for m, _ := range availableMetrics {
+		supports[i] = string(m)
+		i++
+	}
+
+	return &model.DeviceSpecs{
+		Network: *network,
+		Supports: supports,
+	}, nil
+}
---
+++ model/specs.go
@@ -0,0 +1,21 @@
+package model
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type DeviceSpecs struct {
+	Network
+	Supports []string `json:"supports"`
+}
+
+func (ds *DeviceSpecs) Encode() string {
+	return fmt.Sprintf("%s;%s;%s", ds.Hostname, ds.IPAddress, strings.Join(ds.Supports, ","))
+}
+
+func (ds *DeviceSpecs) EncodeJson() string {
+	b, _ := json.Marshal(*ds)
+	return string(b)
+}
