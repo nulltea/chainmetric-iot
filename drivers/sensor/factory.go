163907112a364d677d32665903d4ccf3dea1e048
Timothy <timauthx@gmail.com>
2021-04-24 07:02:03 +0300

    Refactor: implement sensor facrory pattern

---
+++ drivers/sensor/factory.go
@@ -0,0 +1,20 @@
+package sensor
+
+type Factory interface {
+	Build(bus int) Sensor
+}
+
+// FactoryFunc builds Sensor.
+type FactoryFunc func(int) Sensor
+
+// Build calls FactoryFunc to build Sensor on specified peripheral bus
+func (f FactoryFunc) Build(bus int) Sensor {
+	return f(bus)
+}
+
+// I2CFactory provides new factory for building I2C-based Sensor
+func I2CFactory(addr uint16, factory func(addr uint16, bus int) Sensor) Factory {
+	return FactoryFunc(func(bus int) Sensor {
+		return factory(addr, bus)
+	})
+}
--- drivers/sensors/locator.go
+++ drivers/sensors/locator.go
@@ -1,55 +1,41 @@
 package sensors
 
-const (
-	ADXL345_ADDRESS        = 0x53
-	BMP280_ADDRESS         = 0x76
-	CCS811_ADDRESS         = 0x5A
-	HDC1080_ADDRESS        = 0x40
-	MAX30102_ADDRESS       = 0x57
-	MAX44009_ADDRESS       = 0x4B
-	SI1145_ADDRESS         = 0x60
-	LSM303C_A_ADDRESS      = 0x1D
-	LSM303C_M_ADDRESS      = 0x1E
-	ADC_HALL_ADDRESS       = 0x48
-	ADC_PIEZO_ADDRESS      = 0x49
-	ADC_MICROPHONE_ADDRESS = 0x4A
+import (
+	"sort"
+
+	"github.com/timoth-y/iot-blockchain-sensorsys/drivers/sensor"
 )
 
-var I2CSensorsMap = map[uint16]func(bus int) Sensor{
-	ADXL345_ADDRESS: func(bus int) Sensor {
-		return NewADXL345(ADXL345_ADDRESS, bus)
-	},
-	BMP280_ADDRESS: func(bus int) Sensor {
-		return NewBMXX80(BMP280_ADDRESS, bus)
-	},
-	CCS811_ADDRESS: func(bus int) Sensor {
-		return NewCCS811(CCS811_ADDRESS, bus)
-	},
-	HDC1080_ADDRESS:  func(bus int) Sensor {
-		return NewHDC1080(HDC1080_ADDRESS, bus)
-	},
-	MAX30102_ADDRESS: func(bus int) Sensor {
-		return NewMAX30102(MAX44009_ADDRESS, bus)
-	},
-	MAX44009_ADDRESS: func(bus int) Sensor {
-		return NewMAX44009(MAX44009_ADDRESS, bus)
-	},
-	SI1145_ADDRESS: func(bus int) Sensor {
-		return NewSI1145(SI1145_ADDRESS, bus)
-	},
-	LSM303C_A_ADDRESS: func(bus int) Sensor {
-		return NewAccelerometerLSM303(LSM303C_A_ADDRESS, bus)
-	},
-	LSM303C_M_ADDRESS: func(bus int) Sensor {
-		return NewMagnetometerLSM303(LSM303C_M_ADDRESS, bus)
-	},
-	ADC_HALL_ADDRESS: func(bus int) Sensor {
-		return NewADCHall(ADC_HALL_ADDRESS, bus)
-	},
-	ADC_PIEZO_ADDRESS: func(bus int) Sensor {
-		return NewADCPiezo(ADC_PIEZO_ADDRESS, bus)
-	},
-	ADC_MICROPHONE_ADDRESS: func(bus int) Sensor {
-		return NewADCMicrophone(ADC_MICROPHONE_ADDRESS, bus)
-	},
+var i2cSensorsLocatorMap = map[uint16] sensor.Factory {
+	ADXL345_ADDRESS: sensor.I2CFactory(ADXL345_ADDRESS, NewADXL345),
+	BMP280_ADDRESS: sensor.I2CFactory(BMP280_ADDRESS, NewBMXX80),
+	CCS811_ADDRESS: sensor.I2CFactory(CCS811_ADDRESS, NewCCS811),
+	HDC1080_ADDRESS:  sensor.I2CFactory(HDC1080_ADDRESS, NewHDC1080),
+	MAX30102_ADDRESS: sensor.I2CFactory(MAX30102_ADDRESS, NewMAX30102),
+	MAX44009_ADDRESS: sensor.I2CFactory(MAX44009_ADDRESS, NewMAX44009),
+	SI1145_ADDRESS: sensor.I2CFactory(SI1145_ADDRESS, NewSI1145),
+	LSM303C_A_ADDRESS: sensor.I2CFactory(LSM303C_A_ADDRESS, NewAccelerometerLSM303),
+	LSM303C_M_ADDRESS: sensor.I2CFactory(LSM303C_M_ADDRESS, NewMagnetometerLSM303),
+	ADC_HALL_ADDRESS: sensor.I2CFactory(ADC_HALL_ADDRESS, NewADCHall),
+	ADC_PIEZO_ADDRESS: sensor.I2CFactory(ADC_PIEZO_ADDRESS, NewADCPiezo),
+	ADC_MICROPHONE_ADDRESS: sensor.I2CFactory(ADC_MICROPHONE_ADDRESS, NewADCMicrophone),
+}
+
+// LocateI2CSensor locates I2C-based sensor.Sensor and provides its sensor.Factory.
+func LocateI2CSensor(addr uint16) (f sensor.Factory, ok bool) {
+	f, ok = i2cSensorsLocatorMap[addr]
+	return
+}
+
+// I2CAddressesDiapason determines diapason of I2C addresses.
+func I2CAddressesDiapason() (from uint16, to uint16) {
+	var addresses []int
+
+	for addr := range i2cSensorsLocatorMap {
+		addresses = append(addresses, int(addr))
+	}
+
+	sort.Ints(addresses)
+
+	return uint16(addresses[0]), uint16(len(addresses) - 1)
 }
