47f9e0f2fe06715ca32689f2b1a677d58802e8e5
Timothy <timauthx@gmail.com>
2021-04-24 04:36:40 +0300

    Add new ADC-based sensor drrivers to locator

--- drivers/sensors/locator.go
+++ drivers/sensors/locator.go
@@ -1,16 +1,18 @@
 package sensors
 
 const (
-	ADXL345_ADDRESS  = 0x53
-	BMP280_ADDRESS   = 0x76
-	CCS811_ADDRESS   = 0x5A
-	HDC1080_ADDRESS  = 0x40
-	MAX30102_ADDRESS = 0x57
-	MAX44009_ADDRESS = 0x4A
-	SI1145_ADDRESS   = 0x60
-
-	LSM303C_ACCELEROMETER_ADDRESS = 0x1D
-	LSM303C_MAGNETOMETER_ADDRESS  = 0x1E
+	ADXL345_ADDRESS        = 0x53
+	BMP280_ADDRESS         = 0x76
+	CCS811_ADDRESS         = 0x5A
+	HDC1080_ADDRESS        = 0x40
+	MAX30102_ADDRESS       = 0x57
+	MAX44009_ADDRESS       = 0x4B
+	SI1145_ADDRESS         = 0x60
+	LSM303C_A_ADDRESS      = 0x1D
+	LSM303C_M_ADDRESS      = 0x1E
+	ADC_HALL_ADDRESS       = 0x48
+	ADC_PIEZO_ADDRESS      = 0x49
+	ADC_MICROPHONE_ADDRESS = 0x4A
 )
 
 var I2CSensorsMap = map[uint16]func(bus int) Sensor{
@@ -35,11 +37,19 @@ var I2CSensorsMap = map[uint16]func(bus int) Sensor{
 	SI1145_ADDRESS: func(bus int) Sensor {
 		return NewSI1145(SI1145_ADDRESS, bus)
 	},
-	LSM303C_ACCELEROMETER_ADDRESS: func(bus int) Sensor {
-		return NewAccelerometerLSM303(LSM303C_ACCELEROMETER_ADDRESS, bus)
+	LSM303C_A_ADDRESS: func(bus int) Sensor {
+		return NewAccelerometerLSM303(LSM303C_A_ADDRESS, bus)
 	},
-	LSM303C_MAGNETOMETER_ADDRESS: func(bus int) Sensor {
-		return NewMagnetometerLSM303(LSM303C_MAGNETOMETER_ADDRESS, bus)
+	LSM303C_M_ADDRESS: func(bus int) Sensor {
+		return NewMagnetometerLSM303(LSM303C_M_ADDRESS, bus)
 	},
-
+	ADC_HALL_ADDRESS: func(bus int) Sensor {
+		return NewADCHall(ADC_HALL_ADDRESS, bus)
+	},
+	ADC_PIEZO_ADDRESS: func(bus int) Sensor {
+		return NewADCPiezo(ADC_PIEZO_ADDRESS, bus)
+	},
+	ADC_MICROPHONE_ADDRESS: func(bus int) Sensor {
+		return NewADCMicrophone(ADC_MICROPHONE_ADDRESS, bus)
+	},
 }
