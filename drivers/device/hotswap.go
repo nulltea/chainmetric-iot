7c579ade92ba22b0c710de6e9f07afe7f6710e01
Timothy <timauthx@gmail.com>
2021-04-24 07:04:16 +0300

    Add hotswap worker routine init

---
+++ drivers/device/hotswap.go
@@ -0,0 +1,46 @@
+package device
+
+import (
+	"context"
+	"time"
+
+	"github.com/pkg/errors"
+
+	"github.com/timoth-y/iot-blockchain-sensorsys/drivers/periphery"
+	"github.com/timoth-y/iot-blockchain-sensorsys/drivers/sensors"
+	"github.com/timoth-y/iot-blockchain-sensorsys/shared"
+)
+
+func (d *Device) initHotswap() {
+	var (
+		ctx = context.Background()
+		startTime  time.Time
+	)
+
+	ctx, d.cancelHotswap = context.WithCancel(ctx)
+
+	go func() {
+	LOOP:
+		for {
+			startTime = time.Now()
+
+			if err := d.handleHotswap(); err != nil {
+				shared.Logger.Error(errors.Wrap(err, "failed to handle hotswap"))
+			}
+
+			select {
+			case <-time.After(time.Second - time.Since(startTime)):
+			case <- ctx.Done():
+				break LOOP
+			}
+		}
+	}()
+}
+
+func (d *Device) handleHotswap() error {
+	d.i2cScan = periphery.DetectI2C(sensors.I2CAddressesDiapason())
+
+	// TODO: implement hot swap changes for reader
+
+	return nil
+}
